274762ce7d9607bf8e59fd41d1f2bcd9014f098d
olumuyiwa <olumuyiwa.osiname@andela.com>
2016-03-23 11:23:29 +0100

    finish refactor

--- lib/routing/router.rb
+++ lib/routing/router.rb
@@ -4,7 +4,6 @@ module Mgt
       attr_accessor :endpoints
       def draw(&block)
         instance_eval &block
-        #find out the  difference between instance eval and yield
       end
 
       def root(to)
