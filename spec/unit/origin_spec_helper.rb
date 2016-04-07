aaeb91a8b61427f0346767ee0399df5e60d335a2
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-04-07 10:46:35 +0100

    Update origin_spec_helper.rb

--- spec/unit/origin_spec_helper.rb
+++ spec/unit/origin_spec_helper.rb
@@ -1,11 +1,9 @@
 module OriginSpecHelper
   def seed(n = 1)
-    titles = %w(task1 task2 task3 task4 task5 task6 task7 task8 task9 task10)
-    bodies = %w(body1 body2 body3 body4 body5 body6 body7 body8 body9 body10)
-    (0...n).each  do |i|
+    (1..n).each  do |i|
       todo = Todo.new
-      todo.title = titles[i]
-      todo.body =  bodies[i]
+      todo.title = "task#{i}"
+      todo.body =  "body#{i}"
       todo.status = "pending"
       todo.created_at = Time.now.to_s
       todo.save
